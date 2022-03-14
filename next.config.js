43fa8739ce96e83bfb7f20ed7f85d6d048439b99
Maanis <manis16@outlook.com>
2022-03-15 03:16:27 +0500

    final

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '',
+  basePath: '/annual-report-2021',
+  trailingSlash: false,
 }
 
 module.exports = nextConfig
